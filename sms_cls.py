5ec1488bb453639c79cd5b5ddef1654c87c5fe39
KernelA <17554646+KernelA@users.noreply.github.com>
2024-05-05 19:36:39 +0300

    Fix plots

--- sms_cls.py
+++ sms_cls.py
@@ -4,7 +4,7 @@ import pathlib
 import hydra
 import polars as pl
 from omegaconf import DictConfig
-from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.metrics import roc_auc_score
 
 from ods_mlops.sms_cls.model import BaseModel
 
@@ -19,19 +19,12 @@ def main(config: DictConfig):
     for split_type in ("train", "test", "val"):
         predict = model.predict(vect_dir / split_type)
 
-        conf_matrix = confusion_matrix(
-            predict.true_labels,
-            predict.predicted_lables,
-            normalize="all",
-        )
+        class_labels = {v: k for k, v in config.data.class_mapping.items()}
 
-        class_labels = list(
-            map(lambda x: x[0], sorted(config.data.class_mapping.items(), key=lambda x: x[1]))
-        )
-
-        data = pl.from_numpy(
-            conf_matrix,
-            class_labels,
+        data = pl.DataFrame({"actual": predict.true_labels, "predicted": predict.predicted_lables})
+        data = data.with_columns(
+            pl.col("actual").replace(class_labels, return_dtype=pl.String),
+            pl.col("predicted").replace(class_labels, return_dtype=pl.String),
         )
 
         metric_dir = pathlib.Path(config.data.metrics_dir) / split_type
@@ -42,7 +35,7 @@ def main(config: DictConfig):
         with open(metric_dir / "roc_auc.json", "w", encoding="utf-8") as f:
             json.dump({f"{split_type}_roc_auc": roc_auc}, f)
 
-        data.write_json(metric_dir / "conf_matrix.json", row_oriented=True)
+        data.write_csv(metric_dir / "conf_matrix.csv")
 
 
 if __name__ == "__main__":
