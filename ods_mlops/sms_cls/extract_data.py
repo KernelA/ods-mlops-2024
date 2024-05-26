9830f9fc3e8fbffb01fb41c9f3db3cff2197daba
KernelA <17554646+KernelA@users.noreply.github.com>
2024-05-26 21:25:41 +0300

    Add tests

--- ods_mlops/sms_cls/extract_data.py
+++ ods_mlops/sms_cls/extract_data.py
@@ -1,25 +1,17 @@
-import io
+from typing import Iterable
 from unicodedata import normalize
 
 import polars as pl
 
 
-def extract_data(file_object: io.TextIOWrapper, class_mapping: dict) -> pl.DataFrame:
+def extract_data(file_object: Iterable[str], class_mapping: dict) -> pl.DataFrame:
     messages = []
 
     for line in map(str.strip, file_object):
         if not line:
             continue
 
-        index = -1
-        for i, c in enumerate(line):
-            if c == "\t":
-                index = i
-
-        if index == -1:
-            continue
-
-        text, label = line[:index], line[index + 1 :]
+        text, label = line.split("\t")
         messages.append((int(label.strip().lower()), normalize("NFKD", text.strip())))
 
     data = pl.from_records(messages, schema={"target": pl.Int8, "text": pl.String})
